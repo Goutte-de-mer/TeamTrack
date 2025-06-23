15b88d8d11cac5a3782fde24566b7824b7f80e5b
Marion <marionbailleux07@gmail.com>
2025-06-23 14:48:40 +0200

    added models

---
+++ backend/db/models/Task.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  status: {
+    type: String,
+    enum: ["à faire", "en cours", "terminé"],
+    default: "à faire",
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Task", taskSchema);
